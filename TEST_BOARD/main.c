ada78b08ed8d54dff1bdb938c9f424443543f5a2
lactic30 <ghaneshwarsharma30@gmail.com>
2019-09-21 00:39:14 +0530

    AVR(ATMEGA16) board ports testing code

---
+++ TEST_BOARD/main.c
@@ -0,0 +1,21 @@
+/*
+ *
+ * Created(PORTING DATE): 9/19/2019 18:22:36
+ * Author : LACTIC
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void) {
+  /*  DDRA |= (1 << 4); // DDRA &= ~(1<<4);*/
+  DDRC = 0xFF;
+  PORTC = 0x00;
+  /* Replace with your application code */
+  while (1) {
+    PORTC |= 0xFF;
+    _delay_ms(100);
+    PORTC = 0x00;
+    _delay_ms(100);
+  }
+}
