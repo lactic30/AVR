9fad0421cce02725423d3f86ee68473a8750b40c
lactic30 <ghaneshwarsharma30@gmail.com>
2019-09-23 19:52:01 +0530

    reducing time of pir

--- PIR_Inter/main.c
+++ PIR_Inter/main.c
@@ -2,9 +2,8 @@
 #include <util/delay.h>
 
 int main(void) {
-  DDRA = 0x00;  // PIR SENSOR DIRECTION PORT
-  DDRB = 0xFF;  // LED OUTPORT DIRECTION PORT
-  PORTB = 0x00; // LEC PORT INITIAL VALUE
+  DDRA = 0x00; // PIR SENSOR DIRECTION PORT
+  DDRB = 0xFF; // LED OUTPORT DIRECTION PORT
   /* Replace with your application code */
   while (1) {
     PORTB = 0x00;
@@ -12,6 +11,8 @@ int main(void) {
       PORTB = 0x00;
     } else if (bit_is_set(PINA, 0)) {
       PORTB = 0xFF;
+      _delay_ms(100);
+      PORTB = 0x00;
     }
   }
   return 0;
