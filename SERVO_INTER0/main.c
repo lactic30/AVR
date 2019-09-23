e65ba89d7f578fdec99b585e04c2cd017f010d28
lactic30 <ghaneshwarsharma30@gmail.com>
2019-09-23 23:34:02 +0530

    tmier2 testing rather then timer0

--- SERVO_INTER0/main.c
+++ SERVO_INTER0/main.c
@@ -1,16 +1,12 @@
 #include "servo.h"
 
 int main(void) {
-  TimerInit(1);
+  TimerInit(3);
   while (1) {
-    SetOutput(1, 1);
-    _delay_ms(60);
-    SetOutput(1, 5);
-    _delay_ms(60);
-    SetOutput(1, 9);
-    _delay_ms(60);
-    SetOutput(1, 5);
-    _delay_ms(60);
+    for (int i = 3; i <= 19; i++) {
+      SetOutput(3, i);
+      _delay_ms(60);
+    }
   }
   return 0;
 }
